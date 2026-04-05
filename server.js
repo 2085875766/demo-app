cd0769434a29379ee71ed04e41eb6ecf70bd20f4
2085875766 <2085875766@qq.com>
2026-04-06 06:19:45 +0800

    Update app version to v2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-  res.end('hello from myapp\n');
+  res.end('hello from myapp v2\n');
 });
 
 server.listen(3000, () => {
